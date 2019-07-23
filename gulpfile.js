02c0d72e85e1bd8041008099f4a164812f38a48c
Amin Taghipour <amin.taghipour27@gmail.com>
2019-07-24 00:14:41 +0430

    the css and js files concatenate and optimize

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require("gulp");
 const gulpSass = require("gulp-sass");
+const gulpUseref = require("gulp-useref"); // Concatenate Files
+const gulpUglify = require("gulp-uglify"); // Minify JS
+const gulpCssnano = require("gulp-cssnano"); // Minify CSS
+const gulpIf = require("gulp-if");
 
+// The Compile Sass to CSS task
 gulp.task("compileSassToCSS", () => {
   return gulp
     .src("./App/scss/styles.scss")
@@ -8,6 +13,17 @@ gulp.task("compileSassToCSS", () => {
     .pipe(gulp.dest("./App/css"));
 });
 
+// Concatenate and Optimize
+gulp.task("concatAndOptimize", () => {
+  return gulp
+    .src("./App/index.html")
+    .pipe(gulpUseref())
+    .pipe(gulpIf("*.js", gulpUglify()))
+    .pipe(gulpIf("*.css", gulpCssnano()))
+    .pipe(gulp.dest("./dist"));
+});
+
+// Watch Task
 gulp.task(
   "watch",
   gulp.series("compileSassToCSS", () => {
