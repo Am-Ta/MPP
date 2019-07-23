19bb79c4c726f097b3f62678d112c13152b0e8e9
Amin Taghipour <amin.taghipour27@gmail.com>
2019-07-24 01:08:30 +0430

    the images optimize

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const gulp = require("gulp");
 const gulpSass = require("gulp-sass");
-const gulpUseref = require("gulp-useref"); // Concatenate Files
-const gulpUglify = require("gulp-uglify"); // Minify JS
-const gulpCssnano = require("gulp-cssnano"); // Minify CSS
+const gulpUseref = require("gulp-useref"); // the Files concatenate
+const gulpUglify = require("gulp-uglify"); // the JS files minify
+const gulpCssnano = require("gulp-cssnano"); // the CSS files minify
 const gulpIf = require("gulp-if");
+const gulpImagemin = require("gulp-imagemin"); // the images minify
 
 // The Compile Sass to CSS task
 gulp.task("compileSassToCSS", () => {
@@ -13,14 +14,22 @@ gulp.task("compileSassToCSS", () => {
     .pipe(gulp.dest("./App/css"));
 });
 
-// Concatenate and Optimize
-gulp.task("concatAndOptimize", () => {
+// Concatenate and Optimize the CSS and JS files
+gulp.task("concatAndOpt", () => {
   return gulp
     .src("./App/index.html")
     .pipe(gulpUseref())
     .pipe(gulpIf("*.js", gulpUglify()))
     .pipe(gulpIf("*.css", gulpCssnano()))
     .pipe(gulp.dest("./dist"));
+});
+
+// Optimize the images
+gulp.task("imageOpt", () => {
+  return gulp
+    .src("./App/img/*.+(png|jpeg|jpg|svg|gif)")
+    .pipe(gulpImagemin())
+    .pipe(gulp.dest("./dist/img"));
 });
 
 // Watch Task
